65a913f515161bd1629dc89f21f8129164e9d566
azimin <azimin@team.amocrm.com>
2023-08-28 18:41:21 +0300

    chore: Add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@invictus.codes/nuxt-vuetify'
+  ],
+  vuetify: {
+    /* vuetify options */
+    vuetifyOptions: {
+      // @TODO: list all vuetify options
+    },
+
+    moduleOptions: {
+      useVuetifyLabs: true,
+    }
+  }
 })
